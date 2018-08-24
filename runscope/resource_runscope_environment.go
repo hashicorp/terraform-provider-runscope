ef91c31cd0123b3a6280dfbe1a788120556f6c9c
Marek Pastierik <marekpastierik15@gmail.com>
2018-08-24 08:54:28 +0200

    add attribute webhooks and emails

--- runscope/resource_runscope_environment.go
+++ runscope/resource_runscope_environment.go
@@ -83,6 +83,52 @@ func resourceRunscopeEnvironment() *schema.Resource {
 				Optional: true,
 				Default:  true,
 			},
+			"webhooks": &schema.Schema{
+				Type:     schema.TypeSet,
+				Optional: true,
+				Elem:     &schema.Schema{Type: schema.TypeString},
+			},
+			"emails": &schema.Schema{
+				Type: schema.TypeList,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"notify_all": &schema.Schema{
+							Type:     schema.TypeBool,
+							Required: true,
+						},
+						"notify_on": &schema.Schema{
+							Type:     schema.TypeString,
+							Required: true,
+						},
+						"notify_threshold": &schema.Schema{
+							Type:     schema.TypeInt,
+							Required: true,
+						},
+						"recipients": &schema.Schema{
+							Type:  schema.TypeList,
+							Required: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+								"name": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"id": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+								"email": &schema.Schema{
+									Type:     schema.TypeString,
+									Required: true,
+								},
+
+							},
+							},
+						},
+					},
+				},
+				Optional: true,
+			},
 		},
 	}
 }
@@ -155,6 +201,7 @@ func resourceEnvironmentRead(d *schema.ResourceData, meta interface{}) error {
 	d.Set("integrations", readIntegrations(environment.Integrations))
 	d.Set("retry_on_failure", environment.RetryOnFailure)
 	d.Set("verify_ssl", environment.VerifySsl)
+	d.Set("webhooks", environment.WebHooks)
 	return nil
 }
 
@@ -173,7 +220,8 @@ func resourceEnvironmentUpdate(d *schema.ResourceData, meta interface{}) error {
 		d.HasChange("regions") ||
 		d.HasChange("remote_agents") ||
 		d.HasChange("retry_on_failure") ||
-		d.HasChange("verify_ssl") {
+		d.HasChange("verify_ssl") ||
+		d.HasChange("webhooks") {
 		client := meta.(*runscope.Client)
 		bucketID := d.Get("bucket_id").(string)
 		if testID, ok := d.GetOk("test_id"); ok {
@@ -298,6 +346,44 @@ func createEnvironmentFromResourceData(d *schema.ResourceData) (*runscope.Enviro
 	if attr, ok := d.Get("verify_ssl").(bool); ok {
 		environment.VerifySsl = attr
 	}
+
+	if attr, ok := d.GetOk("webhooks"); ok {
+		webhooks := []string{}
+		items := attr.(*schema.Set)
+		for _, x := range items.List() {
+			item := x.(string)
+			webhooks = append(webhooks, item)
+		}
+
+		environment.WebHooks = webhooks
+	}
+
+	if attr, ok := d.GetOk("emails"); ok {
+		contacts := []*runscope.Contact{}
+		tmp := attr.([]interface{})
+		items := tmp[0].(map[string]interface{})
+		emailSettings := runscope.EmailSettings{
+			NotifyAll: items["notify_all"].(bool),
+			NotifyOn:  items["notify_on"].(string),
+			NotifyThreshold: items["notify_threshold"].(int),
+		}
+
+		for _, x := range items["recipients"].([]interface{}) {
+			item := x.(map[string]interface{})
+			contact := runscope.Contact{
+				Name:     item["name"].(string),
+				Email:   item["email"].(string),
+				ID: item["id"].(string),
+			}
+
+			contacts = append(contacts, &contact)
+		}
+
+		emailSettings.Recipients = contacts
+		environment.EmailSettings = &emailSettings
+
+	}
+
 
 	return environment, nil
 }
